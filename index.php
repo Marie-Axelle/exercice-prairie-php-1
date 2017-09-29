a852088c7ce216000ab1eea182cc77f2c63bfb03
Marie-Axelle Simoes <marieaxelles.mas@gmail.com>
2017-09-29 14:09:44 +0200

    exercice php 1

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+$age = 30;
+$nombre = 8;
+$addi = $age + $nombre;
+echo "le resultat est = ";
+echo $addi;
+$multi = $age * $nombre;
+echo "Pour la multiplication c'est =";
+echo $multi;
+ ?>
